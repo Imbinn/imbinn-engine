5379b78550b46d0b3cbd6cea22127c90ab243e6c
Bjarki Madsen <bjerkins8@gmail.com>
2018-04-15 15:44:29 +0200

    cleaning out my closet

--- src/pages/Game/layout.js
+++ src/pages/Game/layout.js
@@ -4,14 +4,12 @@ import { Route } from 'react-router-dom';
 
 import styles from './styles';
 import Users from '../../components/Users';
-import QuizText from '../../components/Rounds/Quiz/Text';
 import RoundLoader from '../../components/Rounds/RoundLoader';
 
 const Game = props => (
     <div style={styles.container}>
         <span>Spilakóði: {props.game.id}</span>
         {props.game.key && <Users users={props.game.users} />}
-        {props.round.key && <QuizText round={props.round} />}
 
         <button>Byrja</button>
 
@@ -22,22 +20,14 @@ const Game = props => (
     </div>
 );
 
-Game.defaultProps = {
-    game: {},
-    round: {},
-};
+Game.defaultProps = { game: {} };
 
 Game.propTypes = {
     game: PropTypes.shape({
         key: PropTypes.string,
         id: PropTypes.string,
         users: PropTypes.shape(),
-    }),
-    round: PropTypes.shape({
-        key: PropTypes.string,
-        type: PropTypes.string,
-        meta: PropTypes.shape(),
-        stages: PropTypes.shape(),
+        rounds: PropTypes.arrayOf(PropTypes.shape()),
     }),
 };
 
