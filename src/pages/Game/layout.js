8953450a2e52a8f67f03c732d13fe87b9cebd24c
Ari Hrannar Björnsson <arihrannar@gmail.com>
2018-04-15 15:54:07 +0200

    removing quiz stuff from game layout

--- src/pages/Game/layout.js
+++ src/pages/Game/layout.js
@@ -4,19 +4,16 @@ import { Route, Link } from 'react-router-dom';
 
 import styles from './styles';
 import Users from '../../components/Users';
-import QuizText from '../../components/Rounds/Quiz/Text';
 import RoundLoader from '../../components/Rounds/RoundLoader';
 
 const Game = props => (
     <div style={styles.container}>
         <span>Spilakóði: {props.game.id}</span>
         {props.game.key && <Users users={props.game.users} />}
-        {props.round.key && <QuizText round={props.round} />}
 
         { props.game.key &&
             <Link to={`/game/${props.game.id}/round/0`}><button>Byrja</button></Link>
         }
-
 
         <Route
             path="/game/:gameId/round/:roundIndex"
@@ -27,7 +24,6 @@ const Game = props => (
 
 Game.defaultProps = {
     game: {},
-    round: {},
 };
 
 Game.propTypes = {
@@ -35,12 +31,7 @@ Game.propTypes = {
         key: PropTypes.string,
         id: PropTypes.string,
         users: PropTypes.shape(),
-    }),
-    round: PropTypes.shape({
-        key: PropTypes.string,
-        type: PropTypes.string,
-        meta: PropTypes.shape(),
-        stages: PropTypes.shape(),
+        rounds: PropTypes.arrayOf(PropTypes.shape()),
     }),
 };
 
